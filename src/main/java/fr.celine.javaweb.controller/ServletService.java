46a26978d1b538345aa15b7f030bfcaf8e6a27a7
celine31 <dadou20231@gmail.com>
2020-07-08 13:43:22 +0200

    ajout classe ServletService avec override de la méthode doGet

---
+++ src/main/java/fr.celine.javaweb.controller/ServletService.java
@@ -0,0 +1,29 @@
+package fr.celine.javaweb.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleServlet extends HttpServlet{
+    private static final String TEST_MESSAGE="SERVER OK"
+
+    public SimpleServlet() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        PrinterWriter out= resp.getWriter();
+        out.println("<h1>" + TEST_MESSAGE + "</h1>");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
