cd704221ecbd5c3734a2dc0ac6e73c448929c22d
celine31 <dadou20231@gmail.com>
2020-07-08 15:00:49 +0200

    le message serveur ok apparait

--- src/main/java/fr.celine.javaweb.controller/ServletService.java
+++ src/main/java/fr.celine.javaweb.controller/ServletService.java
@@ -13,7 +13,7 @@ public class ServletService extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
         resp.setContentType("text/html");
         PrintWriter out= resp.getWriter();
         out.println("<h1>" + TEST_MESSAGE + "</h1>");
